a86d2a1aef2a05a65b62e3f97ab3821cff37213f
Miguel Foliaco <foliaco18@gmail.com>
2024-08-16 08:05:41 -0500

    build(Build): :construction_worker: Add new script to pipeline

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import { ModuleEntry } from './module'
 
 function App() {
-  return ModuleEntry
+  return <ModuleEntry />
 }
 
 export default App
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import { Buffer } from 'buffer'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process': process,
-    'Buffer': Buffer,
-    'Buffer.isBuffer': Buffer.isBuffer
-  },
+  plugins: [react()]
 })
